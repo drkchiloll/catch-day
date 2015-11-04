85c0d369f6bb567038ca6e2deed056ded1671ad2
drkchiloll <samwomack_@me.com>
2015-11-04 09:31:18 -0600

    Added ReactRouter and NotFound Component used when Unknown URLs are entered in the address bar

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,40 @@
 var React = require('react'),
-    ReactDOM = require('react-dom');
+    ReactDOM = require('react-dom'),
+    ReactRouter = require('react-router'),
+    Router = ReactRouter.Router,
+    Route = ReactRouter.Route,
+    Navigation = ReactRouter.Navigation
+    History = require('history/lib/createBrowserHistory');
+
+var history = History();
 
 var App = React.createClass({
   render() {
     return (
       <div className='catch-of-the-day'>
         <div className='menu'>
-          <Header />
+          <Header tagline='Fresh Seafood Market'/>
         </div>
         <Order />
         <Inventory />
       </div>
+    );
+  }
+});
+
+var Header = React.createClass({
+  render() {
+    return (
+      <header className='top'>
+        <h1> Catch
+          <span className='ofThe'>
+            <span className='of'>of</span>
+            <span className='the'>the</span>
+          </span>
+          Day
+        </h1>
+        <h3 className='tagline'><span>{this.props.tagline}</span></h3>
+      </header>
     );
   }
 });
@@ -27,14 +51,6 @@ var Inventory = React.createClass({
   render() {
     return (
       <p> Inventory </p>
-    );
-  }
-});
-
-var Header = React.createClass({
-  render() {
-    return (
-      <p> Header </p>
     );
   }
 });
@@ -56,4 +72,20 @@ var StorePicker = React.createClass({
   }
 });
 
-ReactDOM.render(<App />, document.querySelector('#main'));
+var NotFound = React.createClass({
+  render() {
+    return (
+      <h1>Not Found</h1>
+    );
+  }
+});
+
+var routes = (
+  <Router history={history}>
+    <Route path='/' component={StorePicker} />
+    <Route path='/store/:storeId' component={App} />
+    <Route path='*' component={NotFound} />
+  </Router>
+);
+
+ReactDOM.render(routes, document.querySelector('#main'));
