7a0b924a30f8994ca2e084e89029cbf3a2398007
drkchiloll <samwomack_@me.com>
2015-11-08 08:14:48 -0600

    modified createBrowserHistory to more recognized ES6 Import

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route} from 'react-router';
-import createBrowserHistory from 'history/lib/createBrowserHistory';
+import { createHistory } from 'history';
 import StorePicker from './components/StorePicker';
 import App from './components/App';
 import NotFound from './components/NotFound';
 
-let history = createBrowserHistory();
-
 const routes = (
-  <Router history={history}>
+  <Router history={createHistory()}>
     <Route path='/' component={StorePicker} />
     <Route path='/store/:storeId' component={App} />
     <Route path='*' component={NotFound} />
